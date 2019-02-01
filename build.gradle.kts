c59ec3bf01eebb3393a83a3fe511cf75aabfdd78
soywiz <soywiz@gmail.com>
2019-02-01 06:31:42 +0100

    First version

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+import com.soywiz.korge.gradle.*
+
+buildscript {
+	repositories {
+		mavenLocal()
+		maven { url = uri("https://dl.bintray.com/soywiz/soywiz") }
+		maven { url = uri("https://plugins.gradle.org/m2/") }
+		mavenCentral()
+	}
+	dependencies {
+		classpath("com.soywiz:korge-gradle-plugin:1.0.0")
+	}
+}
+
+apply(plugin = "korge")
+
+korge {
+	id = "com.sample.demo"
+}
