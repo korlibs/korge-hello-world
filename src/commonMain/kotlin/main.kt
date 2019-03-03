51bbe795422df0aa414bb774a2f3e5c8b2d1db34
soywiz <soywiz@gmail.com>
2019-03-03 14:14:50 +0100

    Revert filter

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -2,7 +2,6 @@ import com.soywiz.klock.seconds
 import com.soywiz.korge.*
 import com.soywiz.korge.tween.*
 import com.soywiz.korge.view.*
-import com.soywiz.korge.view.filter.*
 import com.soywiz.korim.color.Colors
 import com.soywiz.korim.format.*
 import com.soywiz.korio.async.launchImmediately
@@ -14,19 +13,17 @@ suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"
 	val minDegrees = (-16).degrees
 	val maxDegrees = (+16).degrees
 
-	val wave = WaveFilter()
 	val image = image(resourcesVfs["korge.png"].readBitmap()) {
 		rotation = maxDegrees
 		anchor(.5, .5)
 		scale(.8)
 		position(256, 256)
-		filter = wave
 	}
 
 	launchImmediately {
 		while (true) {
-			image.tween(image::rotation[minDegrees], wave::time[1.0], time = 1.seconds, easing = Easing.EASE_IN_OUT)
-			image.tween(image::rotation[maxDegrees], wave::time[0.0], time = 1.seconds, easing = Easing.EASE_IN_OUT)
+			image.tween(image::rotation[minDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
+			image.tween(image::rotation[maxDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
 		}
 	}
 }
