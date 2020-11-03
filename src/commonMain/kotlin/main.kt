07caa403d4ef0b301a5afe71638a9da05b24ad64
avdim <avdim@mail.ru>
2020-11-04 00:13:46 +0300

    update code

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -9,7 +9,6 @@ import com.soywiz.korma.geom.*
 import com.soywiz.korma.interpolation.*
 
 suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
-	println("2")
 	val minDegrees = (-16).degrees
 	val maxDegrees = (+16).degrees
 
