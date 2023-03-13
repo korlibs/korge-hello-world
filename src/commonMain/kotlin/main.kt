6f618efef305c69825bd8e6dfe6b669098246b48
soywiz <soywiz@gmail.com>
2023-03-13 15:49:50 +0100

    Update kproject

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -9,7 +9,7 @@ import com.soywiz.korio.file.std.*
 import com.soywiz.korma.geom.*
 import com.soywiz.korma.interpolation.*
 
-suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
+suspend fun main() = Korge(windowSize = SizeInt(512, 512), bgcolor = Colors["#2b2b2b"]) {
 	val sceneContainer = sceneContainer()
 
 	sceneContainer.changeTo({ MyScene() })
