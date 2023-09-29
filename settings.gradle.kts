4646acb0895d6653dddf14eb335f9fa1c36b4d6d
Carlos Ballesteros Velasco <soywiz@gmail.com>
2023-09-29 23:19:29 +0200

    Updates to KorGE 5.0.0 (#27)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,18 @@
-pluginManagement { repositories {  mavenLocal(); mavenCentral(); google(); gradlePluginPortal()  }  }
-
-plugins {
-    //id("com.soywiz.kproject.settings") version "0.0.1-SNAPSHOT"
-    id("com.soywiz.kproject.settings") version "0.3.1"
+pluginManagement {
+    repositories { mavenLocal(); mavenCentral(); google(); gradlePluginPortal() }
 }
 
-kproject("./deps")
+// @TODO: Why can't we just use libs here? We can't even use a TOML parser since it is gradle internal, and we cannot load a library in a reasonably way here.
+plugins {
+    val libsTomlFile = File("gradle/libs.versions.toml").readText()
+    var plugins = false
+    var version = ""
+    for (line in libsTomlFile.lines().map { it.trim() }) {
+        if (line.startsWith("#")) continue
+        if (line.startsWith("[plugins]")) plugins = true
+        if (plugins && line.startsWith("korge") && Regex("^korge\\s*=.*").containsMatchIn(line)) version = Regex("version\\s*=\\s*\"(.*?)\"").find(line)?.groupValues?.get(1) ?: error("Can't find korge version")
+    }
+    if (version.isEmpty()) error("Can't find korge version in $libsTomlFile")
+
+    id("com.soywiz.korge.settings") version version
+}
